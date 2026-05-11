8f3b2314e5b134668f35a9f02c0858eb8d6b545d
Gustavo Resende <gustavo.reseende@gmail.com>
2026-05-11 14:45:20 -0300

    feat: main - injeção de dependências e inicialização

---
+++ cmd/garimpo/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"database/sql"
+	"log/slog"
+	"os"
+	"strconv"
+	"time"
+
+	_ "modernc.org/sqlite"
+
+	"github.com/Gustavo-Resende/garimpo/internal/evolution"
+	"github.com/Gustavo-Resende/garimpo/internal/gemini"
+	"github.com/Gustavo-Resende/garimpo/internal/queue"
+	"github.com/Gustavo-Resende/garimpo/internal/shopee"
+	"github.com/Gustavo-Resende/garimpo/internal/worker"
+)
+
+func mustEnv(key string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		slog.Error("variável de ambiente obrigatória não definida", "key", key)
+		os.Exit(1)
+	}
+	return v
+}
+
+func envFloat(key string, def float64) float64 {
+	v := os.Getenv(key)
+	if v == "" {
+		return def
+	}
+	f, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		slog.Error("variável de ambiente inválida", "key", key, "value", v)
+		os.Exit(1)
+	}
+	return f
+}
+
+func envInt(key string, def int) int {
+	v := os.Getenv(key)
+	if v == "" {
+		return def
+	}
+	i, err := strconv.Atoi(v)
+	if err != nil {
+		slog.Error("variável de ambiente inválida", "key", key, "value", v)
+		os.Exit(1)
+	}
+	return i
+}
+
+func main() {
+	log := slog.Default()
+
+	shopeeAppID      := mustEnv("SHOPEE_APP_ID")
+	shopeeSecret     := mustEnv("SHOPEE_SECRET")
+	geminiAPIKey     := mustEnv("GEMINI_API_KEY")
+	evolutionURL     := mustEnv("EVOLUTION_API_URL")
+	evolutionKey     := mustEnv("EVOLUTION_API_KEY")
+	evolutionInstance := mustEnv("EVOLUTION_INSTANCE_NAME")
+	evolutionGroup   := mustEnv("EVOLUTION_GROUP_JID")
+	dbPath           := mustEnv("DB_PATH")
+
+	minCommission   := envFloat("MIN_COMMISSION", 0.08)
+	extractionHours := envInt("EXTRACTION_INTERVAL_HOURS", 4)
+	postingMinutes  := envInt("POSTING_INTERVAL_MINUTES", 12)
+	startHour       := envInt("POSTING_START_HOUR", 7)
+	endHour         := envInt("POSTING_END_HOUR", 23)
+
+	db, err := sql.Open("sqlite", dbPath)
+	if err != nil {
+		log.Error("abrir banco de dados", "err", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	if err := queue.Migrate(db); err != nil {
+		log.Error("migrate", "err", err)
+		os.Exit(1)
+	}
+
+	q              := queue.NewQueue(db)
+	shopeeClient   := shopee.NewClient(shopeeAppID, shopeeSecret)
+	geminiClient   := gemini.NewClient(geminiAPIKey)
+	evolutionClient := evolution.NewClient(evolutionURL, evolutionKey, evolutionInstance, evolutionGroup)
+
+	extractorCfg := worker.ExtractorConfig{
+		MinCommission:      minCommission,
+		ExtractionInterval: time.Duration(extractionHours) * time.Hour,
+		FetchLimit:         20,
+	}
+	posterCfg := worker.PosterConfig{
+		PostingInterval: time.Duration(postingMinutes) * time.Minute,
+		StartHour:       startHour,
+		EndHour:         endHour,
+	}
+
+	log.Info("garimpo iniciando",
+		"extraction_interval_hours", extractionHours,
+		"posting_interval_minutes", postingMinutes,
+		"min_commission", minCommission,
+	)
+
+	go worker.RunExtractor(shopeeClient, q, extractorCfg, log)
+	go worker.RunPoster(q, geminiClient, evolutionClient, posterCfg, log)
+
+	select {}
+}
