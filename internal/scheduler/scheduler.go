a9d0e9bd5dd8caa3cb0856742fd9d7c62ab0202e
Gustavo Resende <gustavo.reseende@gmail.com>
2026-05-15 20:20:19 -0300

    fix: use fixed timezone BRT-3 instead of LoadLocation

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -3,10 +3,7 @@ package scheduler
 import "time"
 
 func IsWithinWindow(startHour, endHour int) bool {
-	loc, err := time.LoadLocation("America/Bahia")
-	if err != nil {
-		loc = time.UTC
-	}
+	loc := time.FixedZone("BRT", -3*60*60)
 	now := time.Now().In(loc)
 	h := now.Hour()
 	return h >= startHour && h < endHour
